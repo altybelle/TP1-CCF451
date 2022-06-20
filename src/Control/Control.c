02f000c6c20b7edb1c59ed385e4e500b83667b2b
Altamiro Belezia <altamiro.san5@gmail.com>
2022-06-20 18:18:54 -0300

    [Altamiro] Modificação menor

--- src/Control/Control.c
+++ src/Control/Control.c
@@ -46,72 +46,69 @@ int execute_control(){
         write(desc[1], sent, sizeof(sent) + 1);
         exit(0);
         return 1; 
-    } else {
-        struct CPU cpu;
-        struct ExecState exec;
-        struct ReadyState rs;
-        struct BlockedState bs;
-        struct PCB pcb;
-        struct Time time;
-        struct Process u_proc;
-
-        int dequeued;
-
-        if (pipe(s_desc) < 0) {
-            perror("Error: Pipe error.");
-            return -1;
-        }
-        initialize(&cpu, &exec, &rs, &bs, &pcb, &time);
-
-        f = fopen("./FILES/INPUT/PROG.txt", "r");
-
-        if (!f) {
-            puts("Error: coudln't open PROG.txt file.");
-        } else {
-            while((fgets(inst, sizeof(inst), f))){
-                    enqueue_program(&prog, inst);
-                    inst_amount++;
-                }
-            fclose(f);
-        }
-
-        prog.size = inst_amount;
-
-        struct Process proc = create_first_process(&prog, &time, inst_amount, getpid());
-
-        close(desc[1]);
-
-        read(desc[0],  received, sizeof(received));
-        printf("String received from PID %i Manager and sent from Control. Content: '%s'\n\n", getpid(), received);
-
-        enqueue_ready_state(&rs, &proc);
-        enqueue_PCB(&pcb, &proc);
-
-        proc = put_process_CPU(&cpu, &rs);
-        for (int j = 0; j < strlen(received); j++) {
-            switch (received[j]) {
-                case 'I':
-                    print_whole_system(&cpu, &rs, &bs, &pcb);
-                    break;
-                case 'L':
-                    dequeued = dequeue_blocked_state(&bs, &u_proc);
-                    if (dequeued){
-                        enqueue_ready_state(&rs, &u_proc);
-                    }
-                    break;
-                case 'M':
-                    print_average_cycle_time(&time);
-                    exit(0);
-                    break;
-                case 'U':
-                    execute(&cpu, &exec, &rs, &bs, &pcb, &proc, &time);
-                    break;
-                default:
-                    break;
-            }
-        }
-        return 0;
     }
+    
+    int dequeued;
+    struct CPU cpu;
+    struct ExecState exec;
+    struct ReadyState rs;
+    struct BlockedState bs;
+    struct PCB pcb;
+    struct Time time;
+    struct Process u_proc;
+
+    if (pipe(s_desc) < 0) {
+        perror("Error: Pipe error.");
+        return -1;
+    }
+    initialize(&cpu, &exec, &rs, &bs, &pcb, &time);
+
+    f = fopen("./FILES/INPUT/PROG.txt", "r");
+
+    if (!f) {
+        puts("Error: coudln't open PROG.txt file.");
+    } else {
+        while((fgets(inst, sizeof(inst), f))){
+                enqueue_program(&prog, inst);
+                inst_amount++;
+            }
+        fclose(f);
+    }
+
+    prog.size = inst_amount;
+
+    struct Process proc = create_first_process(&prog, &time, inst_amount, getpid());
+
+    close(desc[1]);
+    read(desc[0],  received, sizeof(received));
+    printf("String received from PID %i Manager and sent from Control. Content: '%s'\n\n", getpid(), received);
+    enqueue_ready_state(&rs, &proc);
+    enqueue_PCB(&pcb, &proc);
+
+    proc = put_process_CPU(&cpu, &rs);
+    for (int j = 0; j < strlen(received); j++) {
+        switch (received[j]) {
+            case 'I':
+                print_whole_system(&cpu, &rs, &bs, &pcb);
+                break;
+            case 'L':
+                dequeued = dequeue_blocked_state(&bs, &u_proc);
+                if (dequeued)
+                    enqueue_ready_state(&rs, &u_proc);
+                
+                break;
+            case 'M':
+                print_average_cycle_time(&time);
+                exit(0);
+                break;
+            case 'U':
+                execute(&cpu, &exec, &rs, &bs, &pcb, &proc, &time);
+                break;
+            default:
+                break;
+        }
+    }
+    return 0;
 }
 
 void read_terminal(char *sent){
@@ -133,13 +130,14 @@ int read_file(char *sent){
     if (!f){
       puts("Error: couldn't open CTRL file.");
       return 0;
-    } else {
-      while(fscanf(f, "%s", str) != EOF){
+    } 
+      
+    while(fscanf(f, "%s", str) != EOF) {
         strcat(sent, str);
         strcat(sent, " ");
-      }
-      fclose(f);
     }
+    
+    fclose(f);
     return 1;
 }
 
